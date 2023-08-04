189da64f25be88b0f00d3d47d68d238b0212846a
kqiancc <kqiancc@gmail.com>
2023-08-04 13:17:47 -0400

    search finally works

--- src/routes/home.component.jsx
+++ src/routes/home.component.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Filters from '../components/filters.component';
 import Card from '../components/card.component';
+import MovieCard from '../components/movies-card.component';
 
 const Home = () => {
   const [searchField, setSearchField] = useState('');
@@ -29,13 +30,17 @@ const Home = () => {
 
   return (
     <div className='App'>
-       <h1 className="text-5xl font-bold text-center">saved shows</h1>
-      <div className = "carousel carousel-beginning rounded-box">
-        <div className = "carousel-item">
-        <Card className = "rounded-box"/>
-        </div>
-        </div>
-    </div>
+    <h1 className="text-5xl font-bold text-center padding">search shows</h1>
+   
+   <div className = "carousel carousel-beginning rounded-box">
+     <div className = "carousel-item">
+     <Card className = "rounded-box"/>
+     </div>
+     <div className = "carousel-item">
+     <MovieCard className = "rounded-box"/>
+     </div>
+     </div>
+ </div> 
   );
 };
 
