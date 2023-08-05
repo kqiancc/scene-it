b0516a8a81f5afc8a08a5495651cd8000ad41df7
kqiancc <kqiancc@gmail.com>
2023-08-05 03:03:40 -0400

    fixed the search border thing sort of

--- src/routes/home.component.jsx
+++ src/routes/home.component.jsx
@@ -13,16 +13,22 @@ const Home = () => {
     <div className='App'>
     <h1 className="text-5xl font-bold text-center padding">search shows</h1>
    
-   
+    <div className="flex flex-col w-full border-opacity-50">
+  <div className="grid h-100 card bg-base-300 rounded-box place-items-left">
    <div className = "carousel carousel-beginning rounded-box">
      <div className = "carousel-item">
      <Card className = "rounded-box"/>
      </div>
+     </div>
+     <div className="divider"></div>
+     <div className="grid h-300 card bg-base-300 rounded-box place-items-left">
      <div className = "carousel-item">
      <MovieCard className = "rounded-box"/>
      </div>
      </div>
+     </div>
  </div> 
+ </div>
   );
 };
 
