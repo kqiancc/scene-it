38db12a52fa217d3219164287a981de2f3e5f39f
janeb10 <53449342+janeb10@users.noreply.github.com>
2023-08-15 22:23:26 -0400

    placeholder
    saving to side but have to do w firebase. not clickable still kms

--- src/routes/saved.component.jsx
+++ src/routes/saved.component.jsx
@@ -3,7 +3,8 @@ import DisplayEpisodes from "./display-episodes.component"; // Adjust the import
 
 const Saved = () => {
   const [filterTags, setFilterTags] = useState([]);
-  
+  const [enteredTags, setEnteredTags] = useState(""); // New state for entered tags
+
   return (
     <div>
       <div
@@ -18,18 +19,34 @@ const Saved = () => {
           <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
           <div className="drawer-content">
             <label htmlFor="my-drawer-4" className="drawer-button btn btn-primary">filters</label>
-          </div> 
+            {/* Tags input and Add Tag button */}
+            {/* <input
+              type="text"
+              value={enteredTags}
+              onChange={(e) => setEnteredTags(e.target.value)}
+              onKeyPress={(event) => {
+                if (event.key === "Enter") {
+                  if (enteredTags.trim() !== "") {
+                    const newTag = enteredTags.trim();
+                    setFilterTags((prevTags) => [...prevTags, newTag]);
+                    setEnteredTags(""); // Clear the input after adding the tag
+                  }
+                }
+              }}
+              placeholder="Enter tags"
+              className="w-full max-w-xs input input-ghost input-primary focus:outline-none"
+            /> */}
+          </div>
           <div className="drawer-side">
             <label htmlFor="my-drawer-4" className="drawer-overlay"></label>
             <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
-              
               <h3 className="font-bold mb-2">Filter Tags</h3>
+              {}
               {filterTags.map((tag, index) => (
                 <button
                   key={index}
                   className="btn btn-primary mb-1"
                   onClick={() => {
-                    // Handle tag removal
                     const updatedTags = filterTags.filter((t) => t !== tag);
                     setFilterTags(updatedTags);
                   }}
@@ -37,14 +54,13 @@ const Saved = () => {
                   {tag}
                 </button>
               ))}
-              
-              
+              {/* Sidebar content here */}
             </ul>
           </div>
         </div>
       </div>
-    
-      <DisplayEpisodes filterTags={filterTags} />
+      {}
+      <DisplayEpisodes filterTags={filterTags} enteredTags={enteredTags} />
     </div>
   );
 };
