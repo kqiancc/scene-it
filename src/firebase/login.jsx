acdd72fd37f176082a0d658f8c3fc059ab067faf
kqiancc <kqiancc@gmail.com>
2023-08-18 14:07:42 -0400

    login is clickable and centered

--- src/firebase/login.jsx
+++ src/firebase/login.jsx
@@ -1,14 +1,18 @@
-import { Navigate } from 'react-router-dom';
+import { Outlet, Navigate } from 'react-router-dom';
 import GoogleLogIn from './google-login';
 import Spinner from './spinner';
+import { Fragment } from "react";
+
 
 const Login = ({ user, loading, error }) => {
 	return (
-		<div className='fixed inset-0 flex items-center justify-center mt'>
-			{user && <Navigate to='/' />}
-			{loading ? <Spinner /> : <GoogleLogIn />}
-		</div>
+		<div className='flex items-center justify-center p-0 m-96 h-1/2'>
+    {user && <Navigate to='/' />}
+    {loading ? <Spinner /> : <GoogleLogIn />}
+</div>
+
 	);
 };
+
 
 export default Login;
--- src/firebase/spinner.jsx
+++ src/firebase/spinner.jsx
@@ -2,7 +2,7 @@ import { RiLoader5Line } from 'react-icons/ri';
 
 const Spinner = () => {
     return (
-        <div className='fixed inset-0 flex items-center justify-center'>
+        <div className='flex items-center justify-center p-0 m-96 h-1/2'>
             <RiLoader5Line className='w-10 h-10 animate-spin' />
         </div>
     );
