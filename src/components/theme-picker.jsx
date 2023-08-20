4d37e59d89027119225823b423607001ac1e1ecf
Sean CC <seanlongcc@gmail.com>
2023-08-20 01:28:53 -0400

    mostly works

--- src/components/theme-picker.jsx
+++ src/components/theme-picker.jsx
@@ -1,45 +1,36 @@
+import { useState, useEffect } from "react";
+
 const ThemePicker = () => {
-	return (
-		<div>
-			<button className = "join-item btn">
-				<select
-					className='w-full max-w-xs select select-xs scrollbar'
-					data-choose-theme
-					tabIndex={-1}
-				>
-					<option value='dracula'>dracula</option>
-					<option value='rain'>rain</option>
-					<option value='acid'>acid</option>
-					<option value='aqua'>aqua</option>
-					<option value='autumn'>autumn</option>
-					<option value='bumblebee'>bumblebee</option>
-					<option value='black'>black</option>
-					<option value='business'>business</option>
-					<option value='coffee'>coffee</option>
-					<option value='corporate'>corporate</option>
-					<option value='cupcake'>cupcake</option>
-					<option value='cyberpunk'>cyberpunk</option>
-					<option value='cmyk'>cmyk</option>
-					<option value='dark'>dark</option>
-					<option value='emerald'>emerald</option>
-					<option value='fantasy'>fantasy</option>
-					<option value='forest'>forest</option>
-					<option value='garden'>garden</option>
-					<option value='halloween'>halloween</option>
-					<option value='lemonade'>lemonade</option>
-					<option value='light'>light</option>
-					<option value='lofi'>lofi</option>
-					<option value='luxury'>luxury</option>
-					<option value='night'>night</option>
-					<option value='pastel'>pastel</option>
-					<option value='retro'>retro</option>
-					<option value='synthwave'>synthwave</option>
-					<option value='valentine'>valentine</option>
-					<option value='winter'>winter</option>
-					<option value='wireframe'>wireframe</option>
-				</select>
-			</button>
-		</div>
-	);
+  const [theme, setTheme] = useState("");
+
+  // Toggle theme function
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "rain" ? "dracula" : "rain"));
+  };
+
+  useEffect(() => {
+    // Set theme based on the attribute after the component has mounted
+    const initialTheme =
+      document.documentElement.getAttribute("data-toggle-theme");
+    setTheme(initialTheme);
+    console.log(initialTheme);
+  }, []);
+
+  // useEffect to update the data-theme attribute on the document element
+  useEffect(() => {
+    document.documentElement.setAttribute("data-toggle-theme", theme);
+  }, [theme]);
+
+  return (
+    <button
+      className='btn join-item'
+      data-act-class='ACTIVECLASS'
+      data-toggle-theme='dracula,rain'
+      onClick={toggleTheme}
+    >
+      {theme === "rain" ? "😇" : "😈"}
+    </button>
+  );
 };
+
 export default ThemePicker;
